3ac6112530696902d88c072e111d7d96fff9e1bc
Scott <156713928+slerbert@users.noreply.github.com>
2024-09-21 22:57:48 +1000

    Add UI

--- javascript.js
+++ javascript.js
@@ -1,10 +1,32 @@
-// Generate computer choice
+const scoreboardUser = document.querySelector('.user .score');
+const scoreboardComputer = document.querySelector('.computer .score');
+const resultsContainer = document.querySelector('.results-container');
+const resultsList = document.querySelector('.results-container ol');
+const buttonContainer = document.querySelector('.button-container');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
+let userScore = 0;
+let computerScore = 0;
+
+buttonContainer.addEventListener('click', (event) => {
+    let computerSelection = getComputerChoice();
+    let userSelection = event.target.id;
+
+    switch (event.target.id) {
+        case 'rock':
+        case 'paper':
+        case 'scissors':
+            playRound(userSelection, computerSelection);
+    }
+});
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     return ['rock', 'paper', 'scissors'][choice];
 }
 
-// Get user choice
 function getUserChoice() {
     let choice = prompt('Choose wisely: rock, paper, or scissors?');
     return choice.toLowerCase();
@@ -15,9 +37,6 @@ function toTitleCase(str) {
     let firstLetter = str.charAt(0).toUpperCase();
     return firstLetter + str.slice(1);
 }
-
-let userScore = 0;
-let computerScore = 0;
 
 function playRound(userChoice, computerChoice) {
     let outcome = '';
@@ -61,32 +80,68 @@ function playRound(userChoice, computerChoice) {
         }
     }
 
-    // Print outcome to console and increment score
-    if(outcome == 'win') {
-        console.log(`You win! ${toTitleCase(userChoice)} beats ${computerChoice}.`);
-        userScore++;
-        outcome = '';
-    } else if(outcome == 'loss') {
-        console.log(`You lose! ${toTitleCase(computerChoice)} beats ${userChoice}.`);
-        computerScore++;
-        outcome = '';
-    } else {
-        console.log(`It's a tie! You both chose ${userChoice}.`)
-    }
+    updateResults(outcome, [userChoice, computerChoice]);
+    outcome = '';
+
+    checkGameEnd();
 }
 
 function playGame() {
     let userSelection;
     let computerSelection;
 
-    for(let i = 0; i <= 4; i++) {
-        userSelection = getUserChoice();
-        computerSelection = getComputerChoice();
 
-        playRound(userSelection, computerSelection);
+    // for(let i = 0; i <= 4; i++) {
+    //     userSelection = getUserChoice();
+    //     computerSelection = getComputerChoice();
 
-        console.log(`Your score: ${userScore}/${i+1}        Computer score: ${computerScore}/${i+1}`)
-    }
+    //     playRound(userSelection, computerSelection);
+
+    //     console.log(`Your score: ${userScore}/${i+1}        Computer score: ${computerScore}/${i+1}`)
+    // }
 }
 
-playGame();
+function updateResults(outcome, choices) {
+    resultString = '';
+    const userChoice = choices[0];
+    const computerChoice = choices[1];
+
+    switch (outcome) {
+        case 'win':
+            resultString = `You win! ${toTitleCase(userChoice)} beats ${computerChoice}.`;
+            userScore++;
+            break;
+        case 'loss':
+            resultString = `You lose! ${toTitleCase(computerChoice)} beats ${userChoice}.`;
+            computerScore++;
+            break;
+        case 'tie':
+            resultString = `It's a tie! You both chose ${userChoice}.`;
+            break;
+    }
+
+    updateScoreboard();
+
+    const listItem = document.createElement('li');
+    listItem.textContent = resultString;
+    resultsList.appendChild(listItem);
+}
+
+function updateScoreboard() {
+    scoreboardUser.textContent = userScore;
+    scoreboardComputer.textContent = computerScore;
+}
+
+function checkGameEnd() {
+    if (userScore === 5 || computerScore === 5) {
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+
+        if (userScore === 5) {
+            alert('You win!!!!');
+        } else {
+            alert(`You lost... that's kinda awkward...`);
+        }
+    }
+}
